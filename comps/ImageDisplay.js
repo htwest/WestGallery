4435a53db034574b5d56260f425cf924b7769676
Hayden West <haydenw7@gmail.com>
2021-06-08 17:40:55 -0700

    created useFirestore hook

--- comps/ImageDisplay.js
+++ comps/ImageDisplay.js
@@ -1,4 +1,10 @@
+import useFireStore from "../hooks/useFireStore";
+
 const ImageDisplay = () => {
+  // currently only gets image from 'testImages collection
+  const { docs } = useFireStore("testImages");
+  console.log(docs);
+
   return <div className="img-display">images</div>;
 };
 
