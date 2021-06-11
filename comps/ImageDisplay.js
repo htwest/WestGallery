a33293c6296b897396e30ede81b073ca649b4ad0
Hayden West <haydenw7@gmail.com>
2021-06-11 13:34:05 -0700

    rendered carousel component for each image collection

--- comps/ImageDisplay.js
+++ comps/ImageDisplay.js
@@ -4,11 +4,15 @@ import Carousel from "./Carousel";
 const ImageDisplay = () => {
   // currently only gets image from 'testImages collection
   const { docs } = useFireStore("testImages");
-  console.log(docs);
+  // console.log(docs);
 
   return (
     <div className="img-display">
-      {docs ? docs.map((items) => <Carousel docs={items} />) : null}
+      {docs
+        ? docs.map((items) => (
+            <Carousel docs={items} key={Object.keys(items)[0]} />
+          ))
+        : null}
     </div>
   );
 };
