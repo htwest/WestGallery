20a3e80a336294e0f8e72bcdaa352d24db98258e
Hayden West <haydenw7@gmail.com>
2021-06-14 20:15:33 -0700

    added div

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import ImageDisplay from "../comps/ImageDisplay";
 export default function Home() {
   return (
     <div>
+      <div>WORK IN PROGRESS</div>
       <h1>West-Gallery</h1>
       <UploadForm />
       <ImageDisplay />
