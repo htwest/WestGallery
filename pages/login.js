5886de2b8f06f521063174470e646fd639e32d11
Hayden West <haydenw7@gmail.com>
2021-06-23 13:02:05 -0700

    created login componenet

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router;";
+
+const login = () => {
+  return (
+    <div>
+      <form>
+        <h1>Login</h1>
+        <input type="text" className="username-login"></input>
+        <input type="text" className="password-login"></input>
+        <buttong type="submit">Sumbit</buttong>
+      </form>
+    </div>
+  );
+};
+
+export default login;
