8c32db002cea7b9c5a4837a18d5443e7b3152311
Hayden West <haydenw7@gmail.com>
2021-06-29 12:15:24 -0700

    built out functions for storing email and password in state

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,38 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const login = () => {
-  const redirect = () => {
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("works");
   };
 
   return (
     <div>
-      <form className="login">
+      <form className="login" onSubmit={handleSubmit}>
         <h1>Login</h1>
         <input
           type="text"
           className="username-login"
           placeholder="EMAIL"
+          onChange={handleEmailChange}
         ></input>
         <input
           type="text"
           className="password-login"
           placeholder="PASSWORD"
+          onChange={handlePassChange}
         ></input>
         <button type="submit">Sumbit</button>
       </form>
