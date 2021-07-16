f150d9b016d493ca29d33cdf658a028413a1a98f
Hayden West <haydenw7@gmail.com>
2021-07-16 11:17:16 -0700

    updated header

--- comps/Header.js
+++ comps/Header.js
@@ -2,8 +2,13 @@ const Header = () => {
   return (
     <div className="header">
       <h1>West-Gallery</h1>
-      <p className="quote quote-one">Something, Something</p>
-      <p className="quote quote-two">Insperational...</p>
+      <p className="quote quote-one">
+        I doubted if I should ever come back. I took the one less traveled by,
+      </p>
+      <p className="quote quote-two">
+        {" "}
+        And that has made all the difference...
+      </p>
     </div>
   );
 };
