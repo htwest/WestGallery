7ae5156adeae1cf8f66bd6aea5eac3ed77ec1578
Hayden West <haydenw7@gmail.com>
2021-08-03 19:12:54 -0700

    began work on modal text

--- comps/Modal.js
+++ comps/Modal.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+import ModalText from "./ModalText";
+
 const Modal = ({ selectedImg, setSelectedImg }) => {
+  const [text, setText] = useState(null);
+
   const handleClick = (e) => {
     if (e.target.classList.contains("backdrop")) {
       setSelectedImg(null);
+      setText(null);
+    } else {
+      setText(true);
     }
   };
 
   return (
     <div className="backdrop" onClick={handleClick}>
       <img src={selectedImg} alt="modal-pic" />
+      {text ? <ModalText /> : null}
     </div>
   );
 };
---
+++ comps/ModalText.js
@@ -0,0 +1,9 @@
+const ModalText = () => {
+  return (
+    <div className="modal-text">
+      <p>DOES THIS WORK</p>
+    </div>
+  );
+};
+
+export default ModalText;
