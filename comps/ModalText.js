fc52c2435bc0d31ec3d5770838bda36e935004a1
Hayden West <haydenw7@gmail.com>
2021-08-09 13:52:32 -0700

    fixed image text stylings

--- comps/ModalText.js
+++ comps/ModalText.js
@@ -1,8 +1,10 @@
 const ModalText = ({ selectedStory }) => {
   // console.log(selectedStory);
   return (
-    <div className="modal-text">
-      <p>{selectedStory}</p>
+    <div className="modal-backdrop">
+      <div className="modal-text">
+        <p>{selectedStory}</p>
+      </div>
     </div>
   );
 };
