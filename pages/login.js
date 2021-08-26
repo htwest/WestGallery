8da1f13c9b743084c9fcbce59a6f8bb8ca331e58
Hayden West <haydenw7@gmail.com>
2021-08-26 14:09:24 -0700

    adjusted login stylings

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,9 @@ import { projectAuth } from "../firebase/config";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
+import styles from "../styles/Login.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -35,46 +38,37 @@ const login = () => {
   };
 
   return (
-    <div>
-      <Form className="login" onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <Form.Group className="mb-3">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter Email"
-            onChange={handleEmailChange}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={handlePassChange}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-        {/* <input
-          type="text"
-          className="username-login"
-          placeholder="EMAIL"
-          onChange={handleEmailChange}
-        ></input> */}
-        {/* <input
-          type="text"
-          className="password-login"
-          placeholder="PASSWORD"
-          onChange={handlePassChange}
-        ></input> */}
-        {/* <button type="submit">Sumbit</button> */}
-      </Form>
-      <div className="sign-out">
-        <button className="sign-out-button" onClick={handleSignOut}>
-          SIGN OUT
-        </button>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <Form className="login row mt-5" onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <Form.Group className="col-xs-4">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              onChange={handleEmailChange}
+            />
+          </Form.Group>
+          <Form.Group className="col-xs-4 mb-5">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={handlePassChange}
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+        <div className={styles.signOut}>
+          <Button variant="secondary" onClick={handleSignOut}>
+            SIGN OUT
+          </Button>
+        </div>
       </div>
     </div>
   );
