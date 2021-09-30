969beaa731249d622a27c924b00d7746624fd1bf
Hayden West <haydenw7@gmail.com>
2021-09-30 12:20:40 -0700

    quote now fades in and out on scroll

--- comps/Header.js
+++ comps/Header.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+
 const Header = () => {
+  const [fade, setFade] = useState("header-in");
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleFade);
+    return () => {
+      window.removeEventListener("scroll", handleFade);
+    };
+  }, []);
+
+  const handleFade = () => {
+    let scroll = window.scrollY;
+    if (scroll > 50) {
+      setFade("header-out");
+    }
+    if (scroll < 50) {
+      setFade("header-in");
+    }
+  };
+
   return (
-    <div className="header">
+    <div className={fade}>
       <p className="quote quote-one">Two roads diverged.</p>
       <p className="quote quote-two">I Doubted if I should ever Return</p>
       <p className="quote quote-three">I took the One less traveled by</p>
