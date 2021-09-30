7b2940da644ef6c431810fff4a2e38e39329a3f9
Hayden West <haydenw7@gmail.com>
2021-09-30 16:31:21 -0700

    bug test 2

--- firebase/config.js
+++ firebase/config.js
@@ -6,12 +6,12 @@ import "firebase/storage";
 // Your web app's Firebase configuration
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAjR6KDtycnCxUH26NH1JVJc2Kg6IP3GoY",
-  authDomain: "cjwest-photography.firebaseapp.com",
-  projectId: "cjwest-photography",
-  storageBucket: "cjwest-photography.appspot.com",
-  messagingSenderId: "238541324306",
-  appId: "1:238541324306:web:5a96ae04019263bc0458cb",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESS_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
@@ -30,10 +30,19 @@ const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 export { projectAuth, projectStorage, projectFirestore, timestamp };
 
 // var firebaseConfig = {
-//   apiKey: "AIzaSyAjR6KDtycnCxUH26NH1JVJc2Kg6IP3GoY",
-//   authDomain: "cjwest-photography.firebaseapp.com",
-//   projectId: "cjwest-photography",
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
 //   storageBucket: process.env.STORAGE_BUCKET,
 //   messagingSenderId: process.env.MESS_SENDER_ID,
 //   appId: process.env.APP_ID,
+// };
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAjR6KDtycnCxUH26NH1JVJc2Kg6IP3GoY",
+//   authDomain: "cjwest-photography.firebaseapp.com",
+//   projectId: "cjwest-photography",
+//   storageBucket: "cjwest-photography.appspot.com",
+//   messagingSenderId: "238541324306",
+//   appId: "1:238541324306:web:5a96ae04019263bc0458cb",
 // };
