9750937c869097ef312d1dab675e5ed9b95a0dd3
Hayden West <haydenw7@gmail.com>
2021-09-30 17:07:50 -0700

    env bug test 4

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESS_SENDER_ID: process.env.MESS_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
+};
+
+// env: {
+//   API_KEY: "AIzaSyAjR6KDtycnCxUH26NH1JVJc2Kg6IP3GoY",
+//   AUTH_DOMAIN: "cjwest-photography.firebaseapp.com",
+//   PROJECT_ID: "cjwest-photography",
+//   STORAGE_BUCKET: "cjwest-photography.appspot.com",
+//   MESS_SENDER_ID: "238541324306",
+//   APP_ID: "1:238541324306:web:5a96ae04019263bc0458cb",
+// },
