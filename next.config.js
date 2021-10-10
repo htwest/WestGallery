be47a92beaca9a91328966593199ad24bb5a4bcb
Hayden West <haydenw7@gmail.com>
2021-10-10 13:02:54 -0700

    api bug test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    API_KEY: process.env.API_KEY,
+    API_KEY: "AIzaSyAjR6KDtycnCxUH26NH1JVJc2Kg6IP3GoY",
     AUTH_DOMAIN: process.env.AUTH_DOMAIN,
     PROJECT_ID: process.env.PROJECT_ID,
     STORAGE_BUCKET: process.env.STORAGE_BUCKET,
